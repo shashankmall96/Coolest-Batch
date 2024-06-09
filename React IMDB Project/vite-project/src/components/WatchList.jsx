4f0c9faa262f08fab40d397e083a2d5b1502f23f
Shashank <shashank.mall97@gmail.com>
2024-06-09 12:02:59 +0530

    Genre

--- React IMDB Project/vite-project/src/components/WatchList.jsx
+++ React IMDB Project/vite-project/src/components/WatchList.jsx
@@ -1,56 +1,97 @@
-import React from 'react'
+import React, { useState } from "react";
+import genreids from "../utility/genre";
 
-function WatchList({watchList}) {
+
+
+function WatchList({ watchList }) {
+  const [search, setSearch] = useState("");
+  const [genreList, setGenreList] = useState([]);
+  const [currGenre, setCurrGenre] = useState("All Genres");
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+    //  console.log(e.target)
+  };
+
   return (
     <>
-    
-    
+      {/* Genre Based Filtering */}
 
-    {/* Search Field */}
+      <div className="flex justify-center m-4">
+        {genreList.map((genre) => {
+          return (
+            <div
+              onClick={() => handleFilter(genre)}
+              className={
+                currGenre == genre
+                  ? "mx-4 flex justify-center items-center bg-blue-400 h-[3rem] w-[9rem] text-white font-bold border rounded-xl"
+                  : "flex justify-center items-center h-[3rem] w-[9rem] bg-gray-400/50 rounded-xl text-white font-bold mx-4"
+              }
+            >
+              {genre}
+            </div>
+          );
+        })}
+      </div>
 
-    <div className='flex justify-center my-10'>
-      <input placeholder='Search for movies' className='h-[3rem] w-[18rem] bg-gray-200 px-4 outline-none border border-slate-600' type='text'/>
-    </div>
-    
-    {/* WatchList Table */}
+      {/* Search Field */}
 
-     <div className='m-8'>
+      <div className="flex justify-center my-10">
+        <input
+          placeholder="Search for movies"
+          className="h-[3rem] w-[18rem] bg-gray-200 px-4 outline-none border border-slate-600"
+          type="text"
+          onChange={handleSearch}
+          value={search}
+        />
+      </div>
 
-          <table className='w-full text-center'>
-            <thead className='border border-gray-200 rounded-lg bg-gray-200'>
-              <tr>
+      {/* WatchList Table */}
+
+      <div className="m-8">
+        <table className="w-full text-center">
+          <thead className="border border-gray-200 rounded-lg bg-gray-200">
+            <tr>
               <th>Name</th>
               <th>Ratings</th>
               <th>Popularity</th>
               <th>Genre</th>
               <th>Delete Movies</th>
-              </tr>
-            </thead>
+            </tr>
+          </thead>
 
           <tbody>
-            {watchList.map((movieObj)=>(
-              <tr className='border-b-2'>
-              <td className='flex items-center px-6 py-4'> 
-               <img className='h-[6rem] w-[10rem]' src={`https://image.tmdb.org/t/p/original/${movieObj.poster_path}`}/>
-               <div className='mx-4'>{movieObj.title}</div>
-              </td>
-              <td>{movieObj.vote_average}</td>
-              <td>{movieObj.popularity}</td>
-              <td>Genre</td>
-              <td className='text-red-500'>Delete</td>
-           </tr>
-            ))}
-               
-               
+            {watchList
+              .filter((movieObj) => {
+                if (currGenre == "All Genres") {
+                  return true;
+                } else {
+                  return genreids[movieObj.genre_ids[0]] == currGenre; // Drama;
+                }
+              })
+              .filter((movieObj) =>
+                movieObj.title.toLowerCase().includes(search.toLowerCase())
+              )
+              .map((movieObj) => (
+                <tr className="border-b-2">
+                  <td className="flex items-center px-6 py-4">
+                    <img
+                      className="h-[6rem] w-[10rem]"
+                      src={`https://image.tmdb.org/t/p/original/${movieObj.poster_path}`}
+                    />
+                    <div className="mx-4">{movieObj.title}</div>
+                  </td>
+                  <td>{movieObj.vote_average}</td>
+                  <td>{movieObj.popularity}</td>
+                  <td>{genreids[movieObj.genre_ids[0]]}</td>
+                  <td className="text-red-500">Delete</td>
+                </tr>
+              ))}
           </tbody>
-          </table>
-
-          
-
-     </div>
-    
+        </table>
+      </div>
     </>
-  )
+  );
 }
 
-export default WatchList
+export default WatchList;
