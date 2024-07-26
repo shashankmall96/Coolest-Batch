850e4a0294f3f12ed3bcee5ad340867b40d2c0ed
Shashank <shashank.mall97@gmail.com>
2024-07-26 08:46:33 +0530

    push

---
+++ Redux Notes/index.js
@@ -0,0 +1,46 @@
+import {createStore} from 'redux'
+// Action Constants
+
+const deposit = 'Deposit'
+const withdraw = 'Withdraw'
+
+
+// 1
+const store = createStore(reducer) // 3 set the reducer to the store
+// 2 create Reducer
+
+
+// reducer
+function reducer(state = {amount : 1000 , name:'Mrinal'} , action){
+
+    if(action.type===deposit){
+        return {amount : state.amount+ action.payload , name : state.name}
+    }
+
+    if(action.type===withdraw){
+        return {amount : state.amount - action.payload}
+    }
+    return state
+}
+
+
+console.log(store.getState()) // getting the value of the state
+// How to write Actions
+
+// Action Creators
+function DepositAction(value){
+    return {type : 'Deposit' , payload:value}
+}
+
+function WithdrawAction(value){
+   return {type:'Withdraw' , payload : value}
+   
+}
+
+store.subscribe(()=>{
+    console.log(store.getState())
+})
+
+
+store.dispatch(DepositAction(200)) // dispatching an Action
+store.dispatch(WithdrawAction(300))
