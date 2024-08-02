e02704775285faab5fafca15f1eb99bab04d028d
Shashank <shashank.mall97@gmail.com>
2024-08-02 13:03:56 +0530

    del

---
+++ React Login_SignUp Project/vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import LoginSignup from './Components/LoginSignUp/LoginSignup'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <LoginSignup/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ React Login_SignUp Project/vite-project/src/Components/LoginSignUp/LoginSignup.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './LoginSignup.css'
+import user_icon from '/assets/person.png'
+import email_icon from '/assets/download.png'
+import password_icon from '/assets/password.png'
+
+function LoginSignup() {
+  return (
+    <div className='container'>
+        <div className="header"></div>
+
+
+    </div>
+  )
+}
+
+export default LoginSignup
